9ca1ea8ff1b8a0b09c1a0c6b587cf8164cbb29ad
Dmitry <Valevach.90@mail.ru>
2021-08-07 14:18:39 +0300

    07.08.2021

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,12 +1,54 @@
 package com.efimchick.ifmo.streams.countwords;
 
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Words {
-
     public String countWords(List<String> lines) {
-int a = 0;
-        return null;
+        return lines.stream().map(String::toLowerCase)
+                .map(line->line.split("(?U)[\\pP\\s\\d+]"))
+                .flatMap(array->Arrays.stream(array))
+                .filter(word->word.length()>=4)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry->entry.getValue()>=10L)
+                .sorted(Map.Entry.comparingByValue()
+                        .thenComparing(Comparator.comparing((word1,word2)->{
+
+                        }) {
+            @Override
+            public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                if(o2.getValue().equals(o1.getValue())) {
+                    String s1 = o1.getKey();
+                    String s2 = o2.getKey();
+                    int i = 0;
+                    while (i < s1.length() && i < s2.length()) {
+
+                        if (s1.charAt(i) > s2.charAt(i)) {
+                            return 1;
+
+                        } else if (s1.charAt(i) < s2.charAt(i)) {
+                            return -1;
+
+                        } else if (i == s1.length() - 1) {
+
+                            return -1;
+                        } else if (i == s2.length() - 1) {
+
+                            return 1;
+
+                        } else {
+                            i++;
+                        }
+
+                    }
+                }
+                return (int) (o2.getValue() - o1.getValue());
+            }
+        }).map(entry->entry.getKey() + " - "+entry.getValue())
+                .collect(Collectors.joining("\n"));
+
     }
 }
